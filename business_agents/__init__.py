9ab29c5683c6e79416a38d42278bbb447a081a10
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-06-22 06:10:32 -0600

    Add package exports

---
+++ business_agents/__init__.py
@@ -0,0 +1,5 @@
+"""Velvet autonomous business agent ecosystem."""
+
+from .contracts import AgentProposal, BusinessIntent, ExecutorResult
+
+__all__ = ["AgentProposal", "BusinessIntent", "ExecutorResult"]
